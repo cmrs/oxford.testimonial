d992608213bf2461582836deda2e61130a0a413a
Michael Davis <m.r.davis@me.com>
2011-07-16 17:18:23 +0000

    Update the test base class

--- oxford/testimonial/tests/base.py
+++ oxford/testimonial/tests/base.py
@@ -1,81 +1,36 @@
-"""Test setup for integration and functional tests.
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
 
-When we import PloneTestCase and then call setupPloneSite(), all of
-Plone's products are loaded, and a Plone site will be created. This
-happens at module level, which makes it faster to run each test, but
-slows down test runner startup.
-"""
+from plone.testing import z2
 
-from Products.Five import zcml
-from Products.Five import fiveconfigure
+class TestCase(PloneSandboxLayer):
 
-from Testing import ZopeTestCase as ztc
+    defaultBases = (PLONE_FIXTURE,)
 
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import oxford.testimonial
+        self.loadZCML(package=oxford.testimonial)
 
-# When ZopeTestCase configures Zope, it will *not* auto-load products
-# in Products/. Instead, we have to use a statement such as:
-#   ztc.installProduct('SimpleAttachment')
-# This does *not* apply to products in eggs and Python packages (i.e.
-# not in the Products.*) namespace. For that, see below.
-# All of Plone's products are already set up by PloneTestCase.
+        # Install product and call its initialize() function
+        z2.installProduct(app, 'oxford.testimonial')
 
+        # Note: you can skip this if my.product is not a Zope 2-style
+        # product, i.e. it is not in the Products.* namespace and it
+        # does not have a <five:registerPackage /> directive in its
+        # configure.zcml.
 
-@onsetup
-def setup_product():
-    """Set up the package and its dependencies.
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        self.applyProfile(portal, 'oxford.testimonial:default')
 
-    The @onsetup decorator causes the execution of this body to be
-    deferred until the setup of the Plone site testing layer. We could
-    have created our own layer, but this is the easiest way for Plone
-    integration tests.
-    """
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, 'oxford.testimonial')
 
-    # Load the ZCML configuration for the example.tests package.
-    # This can of course use <include /> to include other packages.
+        # Note: Again, you can skip this if my.product is not a Zope 2-
+        # style product
 
-    fiveconfigure.debug_mode = True
-    import oxford.testimonial
-    zcml.load_config('configure.zcml', oxford.testimonial)
-    fiveconfigure.debug_mode = False
-
-    # We need to tell the testing framework that these products
-    # should be available. This can't happen until after we have loaded
-    # the ZCML. Thus, we do it here. Note the use of installPackage()
-    # instead of installProduct().
-    # This is *only* necessary for packages outside the Products.*
-    # namespace which are also declared as Zope 2 products, using
-    # <five:registerPackage /> in ZCML.
-
-    # We may also need to load dependencies, e.g.:
-    #   ztc.installPackage('borg.localrole')
-
-    ztc.installPackage('oxford.testimonial')
-
-# The order here is important: We first call the (deferred) function
-# which installs the products we need for this product. Then, we let
-# PloneTestCase set up this product on installation.
-
-setup_product()
-ptc.setupPloneSite(products=['oxford.testimonial'])
-
-
-class TestCase(ptc.PloneTestCase):
-    """We use this base class for all the tests in this package. If
-    necessary, we can put common utility or setup code in here. This
-    applies to unit test cases.
-    """
-
-
-class FunctionalTestCase(ptc.FunctionalTestCase):
-    """We use this class for functional integration tests that use
-    doctest syntax. Again, we can put basic common utility or setup
-    code in here.
-    """
-
-    def afterSetUp(self):
-        roles = ('Member', 'Contributor')
-        self.portal.portal_membership.addMember('contributor',
-                                                'secret',
-                                                roles, [])
+OXFORD_TESTIMONIAL_FIXTURE = TestCase()
+OXFORD_TESTIMONIAL_INTEGRATION_TESTING = IntegrationTesting(bases=(OXFORD_TESTIMONIAL_FIXTURE,), name="OxfordTestimonial:Integration")
